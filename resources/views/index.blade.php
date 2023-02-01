33dcf8f132e4e01869d5960b07a5565130f73adf
yakub <mochyakub04@gmail.com>
2023-02-01 14:38:29 +0700

    yakub

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,8 @@
   <section id="hero">
     <div class="hero-container" data-aos="fade-up">
       <h1>Kedok Ombo</h1>
-      <h2>We are team of talented designers making websites with Bootstrap</h2>
-      <a href="#about" class="btn-get-started scrollto">Get Started</a>
+      <h2>Selamat Datang di Company Profile Wisata Desa Gunungrejo</h2>
+      {{-- <a href="#about" class="btn-get-started scrollto">Get Started</a> --}}
     </div>
   </section><!-- End Hero -->
 
